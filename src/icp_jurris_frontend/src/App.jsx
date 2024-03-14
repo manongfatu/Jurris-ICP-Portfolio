cc47e04f421d50cfb012d266f7ae8459a99d89f7
Jurris Leuwe Tolosa <35137572+manongfatu@users.noreply.github.com>
2024-03-14 23:46:31 +0800

    Add files via upload
    initial commit

---
+++ src/icp_jurris_frontend/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <main>
+      <img className="img-dfinity" src="/logo2.svg" alt="DFINITY logo" />
+      <div className="heading-wrapper">
+        <div className="main-heading">
+          <h1>Howdy, Earthling!</h1>
+          <span>Embark on the Ultimate Adventure: Prepare for a Journey Beyond Earth!</span>
+        </div>
+        <iframe className="img-astronaut" src="https://lottie.host/embed/ce53d4ae-9f56-43d4-93e3-1082e25c00c0/vBSMXkktU1.json"></iframe>
+      </div>
+    </main>
+  );
+}
+
+export default App;
---
+++ src/icp_jurris_frontend/src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer>
+      <div className="socials">
+        <a href="https://www.facebook.com/jorislowi" target="_blank">
+          <img className="img-dfinity" src="/icon-facebook.svg" alt="facebook" />
+        </a>
+        <a href="https://twitter.com/jleuwe" target="_blank">
+          <img className="img-dfinity" src="/icon-x.svg" alt="x" />
+        </a>
+        <a href="https://www.instagram.com/jorstloyi/" target="_blank">
+          <img className="img-dfinity" src="/icon-instagram.svg" alt="instagram" />
+        </a>
+      </div>
+      <span className="copyright">Jurris Of Earth | 2024</span>
+    </footer>
+  );
+}
+
+export default Footer;
