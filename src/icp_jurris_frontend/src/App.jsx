77a4dd8ab242d6ae13ff4588a209b3a362972391
Jurris Leuwe Tolosa <jleuwe@Jurriss-MacBook-Pro.local>
2024-03-15 14:46:47 +0800

    Added planets image

--- src/icp_jurris_frontend/src/App.jsx
+++ src/icp_jurris_frontend/src/App.jsx
@@ -11,6 +11,12 @@ function App() {
           <span>Embark on the Ultimate Adventure: Prepare for a Journey Beyond Earth!</span>
         </div>
         <Lottie className="img-astronaut" animationData={astronaut} />
+        <div className="planets">
+          <img className="planet1" src="/planet3.svg" alt="planet1 logo" />
+          <img className="planet2" src="/planet6.svg" alt="planet2 logo" />
+          <img className="planet3" src="/planet7.svg" alt="planet3 logo" />
+          <img className="planet4" src="/planet8.svg" alt="planet4 logo" />
+        </div>
       </div>
     </main>
   );
